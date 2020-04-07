485ff2eb23591a8ca0ef4e068fb513161ecd8a64
Volkan ÇEKİÇ <59247443+volkancekic@users.noreply.github.com>
2020-04-07 19:15:03 +0300

    Add files via upload

---
+++ OrtalamaHesapla/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿/*
+ * SharpDevelop tarafından düzenlendi.
+ * Kullanıcı: V
+ * Tarih: 10.1.2018
+ * Zaman: 11:10
+ * 
+ * Bu şablonu değiştirmek için Araçlar | Seçenekler | Kodlama | Standart Başlıkları Düzenle 'yi kullanın.
+ */
+namespace ortalamahesapla
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button3 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button3.Location = new System.Drawing.Point(164, 144);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(127, 35);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Hesapla";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// label3
+			// 
+			this.label3.BackColor = System.Drawing.SystemColors.HighlightText;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label3.Location = new System.Drawing.Point(70, 194);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(320, 77);
+			this.label3.TabIndex = 5;
+			// 
+			// label4
+			// 
+			this.label4.BackColor = System.Drawing.SystemColors.Highlight;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label4.Location = new System.Drawing.Point(83, 24);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(110, 34);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Vize Notu :";
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label5.Location = new System.Drawing.Point(83, 76);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(119, 35);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "Final Notu :";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.textBox1.Location = new System.Drawing.Point(271, 24);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(60, 23);
+			this.textBox1.TabIndex = 8;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.textBox2.Location = new System.Drawing.Point(271, 76);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(60, 23);
+			this.textBox2.TabIndex = 9;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Highlight;
+			this.ClientSize = new System.Drawing.Size(482, 293);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button3);
+			this.Name = "MainForm";
+			this.Text = "ORTALAMA HESAPLA";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button3;
+	}
+}
---
+++ OrtalamaHesapla/MainForm.cs
@@ -0,0 +1,133 @@
+﻿/*
+ * SharpDevelop tarafından düzenlendi.
+ * Kullanıcı: V
+ * Tarih: 10.1.2018
+ * Zaman: 11:10
+ * 
+ * Bu şablonu değiştirmek için Araçlar | Seçenekler | Kodlama | Standart Başlıkları Düzenle 'yi kullanın.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ortalamahesapla
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			int vize;
+			int final;
+			double ortalama;
+			double but;
+			double kurtarma;
+			string mesaj;
+			
+		
+			
+			
+				if(Convert.ToInt32(textBox1.Text)<0 || Convert.ToInt32(textBox1.Text)>100){
+				mesaj="VİZE NOTUNUZ HATALI LÜTFEN TEKRAR GİRİNİZ.";
+				 MessageBox.Show(mesaj);
+				}
+			
+			vize=Convert.ToInt32(textBox1.Text);
+			
+			   if(Convert.ToInt32(textBox2.Text)<0 || Convert.ToInt32(textBox2.Text)>100){
+			   mesaj="FİNAL NOTUNUZ HATALI LÜTFEN TEKRAR GİRİNİZ.";
+				MessageBox.Show(mesaj);
+			}
+			final=Convert.ToInt32(textBox2.Text);
+			
+			if(vize<0 || vize >100)
+				vize=0;
+			if(final<0 || final >100)
+				final=0;
+			if(final<50){
+				mesaj="FİNAL NOTUNUZ 50'NİN ALTINDA OLDUĞU İÇİN KALDINIZ.";
+			     MessageBox.Show(mesaj);
+		}
+			ortalama=(double)(vize*40)/100 + (final*60)/100;
+			if(ortalama>=59.50 && ortalama<=59.99){
+				ortalama=60;
+			}
+			if(ortalama >=60){
+				if(ortalama>=89.50 ){
+				ortalama=Math.Round(ortalama,0);
+			    mesaj="ORTALAMANIZ \" " +ortalama +" \"   , A1 İLE GEÇTİNİZ. TEBRİKLER.. ";
+			     label3.Text=mesaj;
+				}
+				if(ortalama>=79.50 && ortalama<=89.49){
+					ortalama=Math.Round(ortalama,0);
+			    mesaj="ORTALAMANIZ \" " +ortalama +" \"   , A2 İLE GEÇTİNİZ. TEBRİKLER.. ";
+			     label3.Text=mesaj;
+				}
+				if(ortalama>=69.50 && ortalama<=79.49){
+					ortalama=Math.Round(ortalama,0);
+			    mesaj="ORTALAMANIZ \" " +ortalama +" \"   , B1 İLE GEÇTİNİZ. TEBRİKLER.. ";
+			    label3.Text=mesaj;
+				}
+				if(ortalama>=64.50 && ortalama<=69.49){
+					ortalama=Math.Round(ortalama,0);
+			    mesaj="ORTALAMANIZ \" " +ortalama +" \"   , B2 İLE GEÇTİNİZ. TEBRİKLER.. ";
+			     label3.Text=mesaj;
+				}
+				if(ortalama>=60 && ortalama<=64.49){
+					ortalama=Math.Round(ortalama,0);
+			    mesaj="ORTALAMANIZ  \" " +ortalama +" \"  , C İLE GEÇTİNİZ. TEBRİKLER.. ";
+			     label3.Text=mesaj;
+				}
+			} 
+			 else {
+			   
+				but=60- (vize*40/100);
+				kurtarma=but*100/60;
+				kurtarma=Math.Round(kurtarma,0);
+				if(kurtarma<50){
+					kurtarma=50;
+				}
+				ortalama=Math.Round(ortalama,0);
+			    mesaj="ORTALAMANIZ  \" " +ortalama +" \"  , KALDINIZ.\nGEÇEBİLMENİZ İÇİN BÜTÜNLEME SINAVINDAN  \" " +kurtarma +" \"  ALMANIZ GEREKİYOR.";
+			     label3.Text=mesaj;
+			} 
+			 
+			
+		}
+		
+		void Label1Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
